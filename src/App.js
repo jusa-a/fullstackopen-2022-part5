135c6697775ae3a804d29a0b489302ff791fe224
jusa-a <jusa.annevirta@aalto.fi>
2022-11-07 12:47:49 +0200

    5.1-5.2: bloglist frontend, step1-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs))
+    }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+            blogService.setToken(user.token)
+        }
+    }, [])
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const user = await loginService.login({
+                username,
+                password,
+            })
+
+            window.localStorage.setItem(
+                'loggedBlogappUser',
+                JSON.stringify(user)
+            )
+
+            blogService.setToken(user.token)
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            console.log('Wrong credentials')
+            /* setErrorMessage('Wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000) */
+        }
+    }
+
+    const handleLogout = async () => {
+        window.localStorage.removeItem('loggedBlogappUser')
+        setUser(null)
+        blogService.setToken('')
+    }
+
+    const loginForm = () => (
+        <form onSubmit={handleLogin}>
+            <h2>Log in to application</h2>
+            <div>
+                username{' '}
+                <input
+                    type='text'
+                    value={username}
+                    name='Username'
+                    onChange={({ target }) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+                password{' '}
+                <input
+                    type='text'
+                    value={password}
+                    name='Password'
+                    onChange={({ target }) => setPassword(target.value)}
+                />
+            </div>
+            <button type='submit'>login</button>
+        </form>
+    )
+
+    if (user === null) {
+        return <div>{loginForm()}</div>
+    }
+
+    return (
+        <div>
+            <h2>blogs</h2>
+            {user !== null && (
+                <div>
+                    <p>
+                        {user.name} logged-in{' '}
+                        <button onClick={handleLogout}>logout</button>
+                    </p>
+                </div>
+            )}
+            {blogs.map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+            ))}
+        </div>
+    )
 }
 
 export default App
