45b0c5f07807b900b147d15c597d4069f08d82bd
jusa-a <jusa.annevirta@aalto.fi>
2022-11-08 14:55:20 +0200

    5.9: Blog list frontend, step9

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ const App = () => {
     const [notification, setNotification] = useState({})
 
     useEffect(() => {
-        blogService.getAll().then((blogs) => setBlogs(blogs))
+        blogService
+            .getAll()
+            .then((blogs) => setBlogs(blogs.sort((a, b) => b.likes - a.likes)))
     }, [blogs])
 
     useEffect(() => {
