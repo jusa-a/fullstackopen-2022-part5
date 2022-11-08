3067b965b71c07988e41261e80e360ea5f469253
jusa-a <jusa.annevirta@aalto.fi>
2022-11-08 10:53:28 +0200

    5.7 Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+    const [visible, setVisible] = useState(false)
+
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+    const showWhenVisible = { display: visible ? '' : 'none' }
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+    }
+
+    return (
+        <div style={blogStyle}>
+            <div>
+                {blog.title} {blog.author}{' '}
+                <button style={hideWhenVisible} onClick={toggleVisibility}>
+                    view
+                </button>
+            </div>
+            <div style={showWhenVisible}>
+                <div>{blog.url}</div>
+                <div>
+                    {blog.likes} <button>like</button>
+                </div>
+                <div>{blog.user.name}</div>
+                <button onClick={toggleVisibility}>hide</button>
+            </div>
+        </div>
+    )
+}
 
 export default Blog
